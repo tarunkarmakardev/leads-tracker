0cc50fae13f97ef7db2e5b2bba5ae459475abbaf
Tarun Karmakar <tarun.karmakar.dev@gmail.com>
2025-03-02 23:38:06 +0530

    Client: Update useRefetchQuery

--- apps/client/src/hooks/useRefetchQuery.ts
+++ apps/client/src/hooks/useRefetchQuery.ts
@@ -1,9 +1,11 @@
-import { useQueryClient } from "react-query";
+import { useQueryClient } from "@tanstack/react-query";
 
 const useRefetchQuery = (QUERY_KEY: string) => {
   const qc = useQueryClient();
   return (queryKeys: unknown[] = []) =>
-    qc.invalidateQueries([QUERY_KEY, ...queryKeys]);
+    qc.invalidateQueries({
+      queryKey: [QUERY_KEY, ...queryKeys],
+    });
 };
 
 export default useRefetchQuery;
